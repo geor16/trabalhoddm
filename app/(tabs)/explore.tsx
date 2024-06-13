9d65eba91469b5da207cd26f2eed6c9095e028a9
geor16 <rochafariasgege@gmail.com>
2024-06-13 15:25:22 +0000

    feat-botoes

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,5 +1,5 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import { StyleSheet, Image, Platform, View, TouchableOpacity, Text } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -11,79 +11,66 @@ export default function TabTwoScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      headerImage={<Ionicons size={310} name="people-sharp" style={styles.headerImage} />}>
+      <ThemedText style={styles.text}>Geovana Rocha, 3º INFO, RM 07708</ThemedText>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Redes Sociais</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
+        <ThemedText style={styles.tudo}>
+            <TouchableOpacity style={styles.botao}>
+              <Image
+                source={require('@/assets/images/1.png')}
+                style={styles.logo1}
+              />
+               <Text style={styles.botaoTexto}>GitHub</Text>
+            </TouchableOpacity>
         </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
+        <ThemedText style={styles.tudo}>
+            <TouchableOpacity style={styles.botao2}>
+              <Image
+                source={require('@/assets/images/link.png')}
+                style={styles.logo2}
+              />
+               <Text style={styles.botaoTexto}>Link</Text>
+            </TouchableOpacity>
         </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
+        <ThemedText style={styles.tudo}>
+            <TouchableOpacity style={styles.botao3}>
+              <Image
+                source={require('@/assets/images/yt.png')}
+                style={styles.logo3}
+              />
+               <Text style={styles.botaoTexto}>You</Text>
+            </TouchableOpacity>
         </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
+        <ThemedText style={styles.tudo}>
+            <TouchableOpacity style={styles.botao4}>
+              <Image
+                source={require('@/assets/images/twt.png')}
+                style={styles.logo4}
+              />
+               <Text style={styles.botaoTexto}>Twitter</Text>
+            </TouchableOpacity>
         </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
+        <ThemedText style={styles.tudo}>
+            <TouchableOpacity style={styles.botao5}>
+              <Image
+                source={require('@/assets/images/insta2.png')}
+                style={styles.logo5}
+              />
+               <Text style={styles.botaoTexto}>Instagram</Text>
+            </TouchableOpacity>
         </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user's current color scheme is, and so you can adjust UI colors accordingly.
+        <ThemedText style={styles.tudo}>
+            <TouchableOpacity style={styles.botao6}>
+              <Image
+                source={require('@/assets/images/pin.png')}
+                style={styles.logo6}
+              />
+               <Text style={styles.botaoTexto}>Pinterest</Text>
+            </TouchableOpacity>
         </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText> library
-          to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
+
     </ParallaxScrollView>
   );
 }
@@ -98,5 +85,111 @@ const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
+    paddingBottom: 50,
   },
+  text: {
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'space-around',
+    fontSize: 18,
+    color: "#24A1DE"
+  }, 
+  logo1: {
+    width: 60, 
+    height: 60,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+  botao:{
+    width: 320,
+    height: 60,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    borderRadius: 50,
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  botaoTexto: {
+    color: 'white',
+    marginLeft: 10,
+    fontSize: 40,
+    textAlign: 'center'
+  },
+  logo2: {
+    width: 60, 
+    height: 60,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+  botao2:{
+    width: 320,
+    height: 60,
+    backgroundColor: '#017AB4',
+    justifyContent: 'center',
+    borderRadius: 50,
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  logo3: {
+    width: 60, 
+    height: 60,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+  botao3:{
+    width: 320,
+    height: 60,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    borderRadius: 50,
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  logo4: {
+    width: 60, 
+    height: 60,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+  botao4:{
+    width: 320,
+    height: 60,
+    backgroundColor: '#1D9BF0',
+    justifyContent: 'center',
+    borderRadius: 50,
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  logo5: {
+    width: 60, 
+    height: 60,
+    justifyContent: 'center',
+    paddingLeft: 20,
+  },
+  botao5:{
+    width: 320,
+    height: 60,
+    backgroundColor: '#F91887',
+    justifyContent: 'center',
+    borderRadius: 50,
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+  logo6: {
+    width: 60, 
+    height: 60,
+    justifyContent: 'center',
+    paddingLeft: 20
+  },
+  botao6:{
+    width: 320,
+    height: 60,
+    backgroundColor: '#D2252D',
+    justifyContent: 'center',
+    borderRadius: 50,
+    flexDirection: 'row', 
+    alignItems: 'center',
+  },
+
+
 });
